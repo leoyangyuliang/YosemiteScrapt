ea99e3ecb868e6224e024e9fe6b0eef75b3af5b6
Leo <yangyuliang@csu.fullerton.edu>
2019-02-28 18:18:38 -0800

    send gmail to myself
    gmail notifications managed by this file

---
+++ gmaill.py
@@ -0,0 +1,20 @@
+import smtplib
+import config
+
+def send_email(subject, msg):
+    try:
+        server = smtplib.SMTP('smtp.gmail.com',587)
+        server.ehlo()
+        server.starttls()
+        server.login("config.EMAIL_ADDRESS","config.EMAIL_PASSWARD")
+        message = 'Subject: {}\n\n{}'.format(subject,msg)
+        server.sendmail("config.EMAIL_ADDRESS","config.EMAIL_ADDRESS",message)
+        server.quit()
+        print("success email sent")
+    except:
+        print("something wrong")
+
+subject = "test subject"
+msg = "hello i am fine"
+
+send_email(subject, msg)
